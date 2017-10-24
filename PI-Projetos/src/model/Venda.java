f44d783d6ab19292e3f451cd06cbfa26018f2dac
joao.vrevangelista <joao.vrevangelista@CAS555590W0737.senacsp.edu.br>
2017-10-23 22:27:34 -0200

    venda

---
+++ PI-Projetos/src/model/Venda.java
@@ -0,0 +1,26 @@
+
+package model;
+
+public class Venda {
+    
+    //atributos
+    private int resultado_venda;
+    
+    //construtor
+    public Venda(int resultado_venda){
+        this.resultado_venda = resultado_venda; 
+    }
+    
+    //metodos de acesso 
+    public Integer getResultado_Venda(){
+        return resultado_venda;
+    }
+    public void setResultado_Venda(Integer resultado_venda){
+        this.resultado_venda = resultado_venda ;
+    }
+    
+  
+    
+   
+    
+}
