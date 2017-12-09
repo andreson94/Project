d5d0501b8fcefd76c33e78d54bb978a77f7f824a
joao.vrevangelista <joao.vrevangelista@CAS510513W10B08.senacsp.edu.br>
2017-12-09 14:02:46 -0200

    relatorio

--- src/view/Relatorios.java
+++ src/view/Relatorios.java
@@ -5,18 +5,36 @@
  */
 package view;
 
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import javax.swing.ListSelectionModel;
+import javax.swing.text.DefaultFormatterFactory;
+import javax.swing.text.MaskFormatter;
+import utilitarios.ConectaBanco;
+import utilitarios.ModeloTabela;
+
 /**
  *
  * @author andreson.csilva
  */
 public class Relatorios extends javax.swing.JFrame {
+    ConectaBanco conn = new ConectaBanco();
 
     /**
      * Creates new form Relatorios
      */
     public Relatorios() {
         initComponents();
+        try{
+            MaskFormatter form = new MaskFormatter("##/##/####");
+            txtData.setFormatterFactory(new DefaultFormatterFactory(form));
+        }catch(ParseException ex){
+            
+        }
+        
     }
+ 
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -28,27 +46,32 @@ public class Relatorios extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jLabel2 = new javax.swing.JLabel();
-        jFormattedTextField1 = new javax.swing.JFormattedTextField();
-        jButton1 = new javax.swing.JButton();
+        labelData = new javax.swing.JLabel();
+        txtData = new javax.swing.JFormattedTextField();
+        btnPesquisa = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
-        jLabel8 = new javax.swing.JLabel();
-        jLabel9 = new javax.swing.JLabel();
+        tableRealizada = new javax.swing.JTable();
+        labelVenda = new javax.swing.JLabel();
+        labelDetalhes = new javax.swing.JLabel();
         jScrollPane2 = new javax.swing.JScrollPane();
-        jTable2 = new javax.swing.JTable();
+        tableDetalhe = new javax.swing.JTable();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanel1.setBorder(javax.swing.BorderFactory.createEtchedBorder());
 
-        jLabel2.setText("Data:");
+        labelData.setText("Data:");
 
-        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagens/PESQUISAR.png"))); // NOI18N
-        jButton1.setText("Pesquisa");
+        btnPesquisa.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagens/PESQUISAR.png"))); // NOI18N
+        btnPesquisa.setText("Pesquisa");
+        btnPesquisa.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnPesquisaActionPerformed(evt);
+            }
+        });
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        tableRealizada.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
 
             },
@@ -71,15 +94,20 @@ public class Relatorios extends javax.swing.JFrame {
                 return canEdit [columnIndex];
             }
         });
-        jScrollPane1.setViewportView(jTable1);
+        tableRealizada.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                tableRealizadaMouseClicked(evt);
+            }
+        });
+        jScrollPane1.setViewportView(tableRealizada);
 
-        jLabel8.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        jLabel8.setText("Vendas Realizadas:");
+        labelVenda.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        labelVenda.setText("Vendas Realizadas:");
 
-        jLabel9.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        jLabel9.setText("Detalhe das Venda:");
+        labelDetalhes.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        labelDetalhes.setText("Detalhe das Venda:");
 
-        jTable2.setModel(new javax.swing.table.DefaultTableModel(
+        tableDetalhe.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
 
             },
@@ -102,7 +130,7 @@ public class Relatorios extends javax.swing.JFrame {
                 return canEdit [columnIndex];
             }
         });
-        jScrollPane2.setViewportView(jTable2);
+        jScrollPane2.setViewportView(tableDetalhe);
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -112,15 +140,15 @@ public class Relatorios extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 626, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel8)
+                    .addComponent(labelVenda)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jLabel2)
+                        .addComponent(labelData)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jFormattedTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 171, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(txtData, javax.swing.GroupLayout.PREFERRED_SIZE, 171, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jButton1))
+                        .addComponent(btnPesquisa))
                     .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 626, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel9))
+                    .addComponent(labelDetalhes))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
@@ -128,15 +156,15 @@ public class Relatorios extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(20, 20, 20)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2)
-                    .addComponent(jFormattedTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton1))
+                    .addComponent(labelData)
+                    .addComponent(txtData, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnPesquisa))
                 .addGap(18, 18, 18)
-                .addComponent(jLabel8)
+                .addComponent(labelVenda)
                 .addGap(10, 10, 10)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel9)
+                .addComponent(labelDetalhes)
                 .addGap(10, 10, 10)
                 .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
@@ -171,6 +199,73 @@ public class Relatorios extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void btnPesquisaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPesquisaActionPerformed
+        // TODO add your handling code here:
+        preencherTabela("select * from venda inner join clientes on venda.id_cliente = clientes.id_cliente where data_venda='" +txtData.getText()+"'" );
+    }//GEN-LAST:event_btnPesquisaActionPerformed
+
+    private void tableRealizadaMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tableRealizadaMouseClicked
+        // TODO add your handling code here:
+        int cod = (int) tableRealizada.getValueAt(tableRealizada.getSelectedRow(), 0);
+        preencherTableDetalhes("select * from venda inner join itens_venda_produto on venda.id_venda = itens_venda_produto.id_venda inner join produto on itens_venda_produto.id_produto = produto.id_produto where venda.id_venda="+cod);
+        
+    }//GEN-LAST:event_tableRealizadaMouseClicked
+     public void preencherTableDetalhes(String SQL){
+         ArrayList dados = new ArrayList();
+         String [] Colunas = new String[]{"Produto","Quantidade"};
+         conn.conexao();
+         conn.executaSQL(SQL);
+         try{
+             conn.rs.first();
+             do{
+                 dados.add(new Object[]{conn.rs.getString("nome_produto"), conn.rs.getString("quantidade_produto")});
+             }while(conn.rs.next());
+         }catch(SQLException ex){
+             
+         }
+         ModeloTabela modelo = new ModeloTabela(dados, Colunas);
+         tableDetalhe.setModel(modelo);
+         tableDetalhe.getColumnModel().getColumn(0).setPreferredWidth(200);
+         tableDetalhe.getColumnModel().getColumn(0).setResizable(false);
+         tableDetalhe.getColumnModel().getColumn(1).setPreferredWidth(100);
+         tableDetalhe.getColumnModel().getColumn(1).setResizable(false);
+         tableDetalhe.getTableHeader().setReorderingAllowed(false);
+         tableDetalhe.setAutoResizeMode(tableDetalhe.AUTO_RESIZE_OFF);
+         tableDetalhe.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+         
+     }
+         public void preencherTabela(String SQL){
+        ArrayList dados = new ArrayList();
+        
+        String[] Colunas = new String[]{"ID","Data Venda", "Valor da Venda", "Cliente"};
+        conn.conexao();
+        conn.executaSQL(SQL);
+        try{
+            conn.rs.first();
+            do{
+                dados.add(new Object[]{conn.rs.getInt("id_venda"), conn.rs.getString("data_venda")});
+            }while(conn.rs.next());
+        }catch(SQLException ex){
+            
+        }
+        ModeloTabela modelo = new ModeloTabela(dados, Colunas);
+        tableRealizada.setModel(modelo);
+        tableRealizada.getColumnModel().getColumn(0).setPreferredWidth(50);
+        tableRealizada.getColumnModel().getColumn(0).setResizable(false);
+        tableRealizada.getColumnModel().getColumn(1).setPreferredWidth(100);
+        tableRealizada.getColumnModel().getColumn(1).setResizable(false);
+        tableRealizada.getColumnModel().getColumn(2).setPreferredWidth(100);
+        tableRealizada.getColumnModel().getColumn(2).setResizable(false);
+        tableRealizada.getColumnModel().getColumn(3).setPreferredWidth(250);
+        tableRealizada.getColumnModel().getColumn(3).setResizable(false);
+        tableRealizada.getTableHeader().setReorderingAllowed(false);
+        tableRealizada.setAutoResizeMode(tableRealizada.AUTO_RESIZE_OFF);
+        tableRealizada.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        
+        
+    }
+     
+    
     /**
      * @param args the command line arguments
      */
@@ -207,16 +302,16 @@ public class Relatorios extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JFormattedTextField jFormattedTextField1;
+    private javax.swing.JButton btnPesquisa;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel8;
-    private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JTable jTable1;
-    private javax.swing.JTable jTable2;
+    private javax.swing.JLabel labelData;
+    private javax.swing.JLabel labelDetalhes;
+    private javax.swing.JLabel labelVenda;
+    private javax.swing.JTable tableDetalhe;
+    private javax.swing.JTable tableRealizada;
+    private javax.swing.JFormattedTextField txtData;
     // End of variables declaration//GEN-END:variables
 }
